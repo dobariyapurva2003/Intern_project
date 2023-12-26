7c8007814f2f71bde15a475a0f58fb8fd22c2486
dobariyapurva2003 <purvadobariya9@gmail.com>
2023-12-26 18:50:58 +0530

    Testing commit

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
     return (
         <>
-            <div class="text-center font-bold text-red-500 text-8xl">Hello from Home</div>
+            <div class="text-center font-bold text-red-500 text-8xl">Hello from Home welcome</div>
         </>
     )
 }
