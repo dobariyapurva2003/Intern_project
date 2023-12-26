dd117d69297f5f53f74d1566f2308fce66ec5880
dobariyapurva2003 <purvadobariya9@gmail.com>
2023-12-26 18:37:26 +0530

    Initial commit

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <>
+            <div class="text-center font-bold text-red-500 text-8xl">Hello from Home</div>
+        </>
+    )
+}
+
+export default Home;
